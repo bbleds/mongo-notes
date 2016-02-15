6fc950f4c037fd2d778629bc99a7266ad34ab329
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-15 13:15:13 -0600

    configuring package.json file

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 
 app.get("/", (req, res)=>
 {
-	res.send("HALLO, this is thy index page")
+	res.send("<h1>HALLO, this is thy index page</h1>")
 })
 
-app.listen(3000, ()=>
+
+app.get("/contact", (req, res)=>
+{
+	res.send("<h1>HALLO, this is thy contact page</h1>")
+})
+
+app.listen(PORT, ()=>
 {
 	console.log("app listening on PORT 3000");
 })
